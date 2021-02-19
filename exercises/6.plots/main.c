bff70088f84c5ec48c19556346f6361d7adc7e93
alfiehopkinson <a.hopkinson@phy.au.dk>
2021-02-19 13:59:13 +0000

    19/2

--- exercises/6.plots/main.c
+++ exercises/6.plots/main.c
@@ -2,10 +2,12 @@
 #include<math.h>
 #include<gsl/gsl_sf_erf.h>
 
+double Erf(double);
+
 int main(){
 	double xmin =-2, xmax=2;
 	for(double x=xmin; x<=xmax; x+=1.0/8){
-		printf("%10g %10g %10g\n",x,erf(x),gsl_sf_erf(x));
+		printf("%10g %10g %10g %10g\n",x,erf(x),gsl_sf_erf(x),Erf(x));
 		}
 return 0;
 }
