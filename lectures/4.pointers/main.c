bb0a46108a11629947c44d180afa9e45010577a2
alfiehopkinson <a.hopkinson@phy.au.dk>
2021-03-01 13:13:27 +0000

    01/3

--- lectures/4.pointers/main.c
+++ lectures/4.pointers/main.c
@@ -4,8 +4,8 @@
 #include<assert.h>
 
 //struct vector {int n, double a[]};
-//struct vector my_vector;
-//typedef struct ector vector
+////struct vector my_vector;
+////typedef struct ector vector
 
 void set0(double x){x=0;}
 
@@ -21,7 +21,7 @@ int main(){
 	printf("y after set0 = %g\n", y);
 	set0p(&y);
 	printf("y after set0p = %g\n", y);
-	
+								
 	int n=5;
 	double v[5]; // define arrays like [] goes from 0to n-1
 	for(int i=0;i<n;i++){
@@ -33,7 +33,7 @@ int main(){
 	}
 	//if we put v[9999999]=1.0; it would be too large and push it over into a segmentation limit
 	print_array(n,v);
-	
+																					
 	int N=7;
 	double *a = malloc(N*sizeof(double)); //this asks a pointer to keep engough memeery allocated. its the same as the one above
 	for(int i=0;i<N;i++) a[i]=i+100; //all of this memory is allocated manually and so has to be deleted
