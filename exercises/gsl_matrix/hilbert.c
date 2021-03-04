37120749ca87b2b4742219fec1164d2f397f2a8f
alfiehopkinson <a.hopkinson@phy.au.dk>
2021-03-04 11:28:51 +0000

    04/3

--- exercises/gsl_matrix/hilbert.c
+++ exercises/gsl_matrix/hilbert.c
@@ -15,14 +15,28 @@ void vector_print(char s[], gsl_vector* v){
 	printf("\n");
 }
 
+void print_matrix(char s[], gsl_matrix* mat) {
+	printf("%s\n", s);
+	for(int i=0; i<mat->size1; i++) {
+		for(int j=0; j<mat->size2; j++) {
+			printf("%10g", gsl_matrix_get(mat,i,j));
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+
+
+
+
 
 int main(){
 	int n=2;
 	gsl_matrix* A=gsl_matrix_alloc(n,n);
 	gsl_matrix* Acopy=gsl_matrix_alloc(n,n);
 	gsl_vector* eval=gsl_vector_calloc(n);
-	gsl_eigen_symm_workspace * W=gsl_eigen_symm_alloc(n); 	
-
+	gsl_matrix* evec=gsl_matrix_alloc(n,n);
+	gsl_eigen_symmv_workspace * W=gsl_eigen_symmv_alloc(n);
 
 
 	gsl_matrix_set(A,0,0,1.0);
@@ -34,10 +48,11 @@ int main(){
 
 	gsl_matrix_memcpy(Acopy,A);	
  																			
-	gsl_eigen_symm(A,eval,W);
+//	gsl_eigen_symm(A,eval,W);
+	gsl_eigen_symmv(A,eval,evec,W);
 
-
-	vector_print("eigen values are =",eval);	
+	vector_print("eigen values are =",eval);
+	print_matrix("The eigen vectors are =",evec);	
 
 
 	gsl_matrix_free(A);
