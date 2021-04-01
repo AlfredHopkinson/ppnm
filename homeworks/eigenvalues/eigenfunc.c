46c45631cffa8140e22f21c307394536980657bd
alfiehopkinson <a.hopkinson@phy.au.dk>
2021-04-01 11:41:21 +0100

    Part A complete

--- homeworks/eigenvalues/eigenfunc.c
+++ homeworks/eigenvalues/eigenfunc.c
@@ -19,7 +19,7 @@ void print_matrix(const char* p, gsl_matrix * A){
 	printf("%s\n",p);
 	for(int i=0;i<A->size1;i++){
 		for(int j=0;j<A->size2;j++){
-			printf("%10g",gsl_matrix_get(A,i,j));
+			printf("%15g",gsl_matrix_get(A,i,j));
 		}
 		printf("\n");
 	}
@@ -27,51 +27,47 @@ void print_matrix(const char* p, gsl_matrix * A){
 
 //something like in the example 
 
-void timesJ(gsl_matrix* A, int p, int q, double omega){
-	double c=cos(omega),s=sin(omega);
+void timesJ(gsl_matrix* A, int p, int q, double theta){
+	double c=cos(theta),s=sin(theta);
 	for(int i=0;i<A->size1;i++){
-		double newA_ip=c*gsl_matrix_get(A,i,p)-s*gsl_matrix_get(A,i,q);
-		double newA_iq=s*gsl_matrix_get(A,i,p)+c*gsl_matrix_get(A,i,q);
-		gsl_matrix_set(A,i,p,newA_ip);
-		gsl_matrix_set(A,i,q,newA_iq);
+		double new_aip=c*gsl_matrix_get(A,i,p)-s*gsl_matrix_get(A,i,q);
+		double new_aiq=s*gsl_matrix_get(A,i,p)+c*gsl_matrix_get(A,i,q);
+		gsl_matrix_set(A,i,p,new_aip);
+		gsl_matrix_set(A,i,q,new_aiq);
 	}
 }
 
-void Jtimes(gsl_matrix* A, int p, int q, double omega){
-	double c=cos(omega),s=sin(omega);
+void Jtimes(gsl_matrix* A, int p, int q, double theta){
+	double c=cos(theta),s=sin(theta);
 	for(int j=0;j<A->size2;j++){
-		double newA_pj= c*gsl_matrix_get(A,p,j)+s*gsl_matrix_get(A,q,j);
-		double newA_qj=-s*gsl_matrix_get(A,p,j)+c*gsl_matrix_get(A,q,j);
-		gsl_matrix_set(A,p,j,newA_pj);
-		gsl_matrix_set(A,q,j,newA_qj);
+		double new_apj= c*gsl_matrix_get(A,p,j)+s*gsl_matrix_get(A,q,j);
+		double new_aqj=-s*gsl_matrix_get(A,p,j)+c*gsl_matrix_get(A,q,j);
+		gsl_matrix_set(A,p,j,new_apj);
+		gsl_matrix_set(A,q,j,new_aqj);
 	}
 }
 
 void jacobi_diag (gsl_matrix * A, gsl_matrix * V){
 	int changed;
-	int n = A->size1;
-	double omega , c, s;
-	double apq, aqq, app, new_app, new_aqq;
-
+	int n = A->size1;	
+	
 	do{
 		changed=0;
 		for(int p=0;p<n-1;p++){
-			for(int q=p+1;q<n;q++){
-
-				apq=gsl_matrix_get(A,p,q);
-				app=gsl_matrix_get(A,p,p);
-				aqq=gsl_matrix_get(A,q,q);
-				omega=0.5*atan(2*apq/(aqq-app));
-				c=cos(omega),s=sin(omega);
-		
-				new_app=c*c*app-2*s*c*apq+s*s*aqq;
-				new_aqq=s*s*app+2*s*c*apq+c*c*aqq;
-				if(new_app!=app || new_aqq!=aqq) // do rotation
-					{
-					changed=1;
-					timesJ(A,p,q, omega);
-					Jtimes(A,p,q,-omega); // A←J^T*A*J 
-					timesJ(V,p,q, omega); // V←V*J
+		for(int q=p+1;q<n;q++){
+			double apq=gsl_matrix_get(A,p,q);
+			double app=gsl_matrix_get(A,p,p);
+			double aqq=gsl_matrix_get(A,q,q);
+			double theta=0.5*atan2(2*apq,aqq-app);
+			double c=cos(theta),s=sin(theta);
+			double new_app=c*c*app-2*s*c*apq+s*s*aqq;
+			double new_aqq=s*s*app+2*s*c*apq+c*c*aqq;
+			if(new_app!=app || new_aqq!=aqq) 
+				{
+				changed=1;
+				timesJ(A,p,q, theta);
+				Jtimes(A,p,q,-theta); 
+				timesJ(V,p,q, theta);
 				}
 			}
 		}
--- homeworks/eigenvalues/main.c
+++ homeworks/eigenvalues/main.c
@@ -14,26 +14,65 @@ int main(){
 	printf("PART A\n");
 	printf("Proving my jacobi implimentation works\n");
 	
-	double n = 4;
+	double n = 6;
 	//create the random matrix A from my previous code
 	gsl_matrix* A=gsl_matrix_alloc(n,n);
 	gsl_matrix* D=gsl_matrix_alloc(n,n);
 	gsl_matrix* V=gsl_matrix_alloc(n,n);
+	
 	for(int i=0;i<A->size1;i++){
 		for(int j=0;j<A->size2;j++){
-			gsl_matrix_set(A,i,j,rand()%10);
-			gsl_matrix_set(V,i,j,0); //had a bit of trouble here as needed to make V a diagonal
+			//gsl_matrix_set(A,i,j,rand()%10);  Usual random method not working to make it symmetric going to try an allocate a double and put it in this loop
+			double part = rand()%10;
+			gsl_matrix_set(A,i,j,part);
+			gsl_matrix_set(A,j,i,part);
+	
 		}
-		gsl_matrix_set(V,i,i,1);
 	}
 	print_matrix("A=",A);
 	//make a copy
 	gsl_matrix_memcpy(D,A);
+	gsl_matrix_set_identity(V); //had a bit of trouble here as needed to make V a diagonal
 	//now I have A and V i can do the diagonalisation 
 	print_matrix("V=",V);
 	jacobi_diag(D,V);
 	print_matrix("D=",D);
 	print_matrix("V=",V);
+
+
+	//now I have to check the implimentation as in the question 
+	//I find AV*At =D
+	printf("I will firstly show V^tAV==D\n");
+	//times in 2 parts saving the first into e
+	gsl_matrix* e=gsl_matrix_alloc(n,n);
+	gsl_matrix* vtav=gsl_matrix_alloc(n,n);
+	gsl_blas_dgemm(CblasNoTrans,CblasNoTrans,1.0,A,V,0.0,e);
+	gsl_blas_dgemm(CblasTrans,CblasNoTrans,1.0,V,e,0.0,vtav);
+
+	print_matrix("D=",D);
+	print_matrix("V^t AV =",vtav);
+
+	printf("I will now show VDV^t==A\n");
+	//do same as before
+	gsl_matrix* r=gsl_matrix_alloc(n,n);
+	gsl_matrix* vdvt=gsl_matrix_alloc(n,n);
+	gsl_blas_dgemm(CblasNoTrans,CblasNoTrans,1.0,V,D,0.0,r);
+	gsl_blas_dgemm(CblasNoTrans,CblasTrans,1.0,r,V,0.0,vdvt);
+
+	print_matrix("A=",A);
+	print_matrix("VDV^t=",vdvt);
+
+	printf("I will now show VV^t==i\n");
+
+	gsl_matrix* VV=gsl_matrix_alloc(n,n);
+	gsl_blas_dgemm(CblasTrans,CblasNoTrans,1.0,V,V,0.0,VV);
+	print_matrix("V^t V=",VV);
+
+	printf("all the checks confirm the implimentation works\n");
+
+
+
+
 
 
 return 0;
