7d7e8c219581abd97edbdeb9c3ea1c60bed1b962
alfiehopkinson <a.hopkinson@phy.au.dk>
2021-04-07 11:26:39 +0100

    part a

--- homeworks/ODE/func.c
+++ homeworks/ODE/func.c
@@ -25,6 +25,57 @@ void print_matrix(const char* p, gsl_matrix * A){
 	}
 }
 
+//make the RK stepper we go for 12 like in the example
+void rkstep12(
+	//compared to the exampel x=t, yx=yt, h=h, yh=yh, dy= err
+	void f(double t,gsl_vector*y,gsl_vector*dydt) /* the f from dy/dt=f(t,y) */
+	double t,              /* the current value of the variable */
+	gsl_vector* yt,            /* the current value y(t) of the sought function */
+	double h,              /* the step to be taken */
+	gsl_vector* yh,             /* output: y(t+h) */
+	gsl_vector* err){
+
+	int c = yt->size;
+	//dont need to alloc yt as its called above and alloced in main
+	gsl_vector* k0 = gsl_vector_alloc(c);
+	gsl_vector* k12 = gsl_vector_alloc(c);
+	gsl_vector* ytt = gsl_vector_alloc(c);
+
+
+	f(c,t,yt,k0); // this takes the step and evaluates 
+	for(int i = 0; i<c; i++){
+		gsl_vector_set(yt,i, gsl_vector_get(yt)+gsl_vector_get(k0,i)*0.5*h);
+	}
+	f(c,t+0.5*h,yh,k12);
+	for(int i=0; i<c; i++){
+		gsl_vector_set(yh,i, gsl_vector_get(yt)+gsl_vector_get(k12,i)*h);
+	}
+	for(int i=0;i<c;i++){
+		gsl_vector_set(err,i, (gsl_vector_get(k0,i)-gsl_vector_get(k12,i))*0.5*h;
+	}
+	gsl_vector_free(k0);
+	gsl_vector_free(k12);
+
+}
+
+//now time to do the second driver part
+
+void driver(
+	void f(double,vector*,vector*), /* right-hand-side of dy/dt=f(t,y) */
+	double a,                     /* the start-point a */
+	vector* ya,                     /* y(a) */
+	double b,                     /* the end-point of the integration */
+	vector* yb,                     /* y(b) to be calculated */
+	double h,                     /* initial step-size */
+	double acc,                   /* absolute accuracy goal */
+	double eps
+){ 
+
+
+
+
+
+
 
 
 
